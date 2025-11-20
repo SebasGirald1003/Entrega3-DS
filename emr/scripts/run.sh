6a0df41ebe817bf455e7e8ba65087c20f38a04be
jjvasquezg <juanjos137k@gmail.com>
2025-11-20 18:01:40 -0500

    Nuevas imagenes y modificación del run.sh

--- emr/scripts/run.sh
+++ emr/scripts/run.sh
@@ -67,17 +67,15 @@ while true; do
           break
           ;;
       FAILED|TIMEOUT)
-          echo "❌ Glue Job falló con estado: $STATUS"
+          echo "Glue Job falló con estado: $STATUS"
           exit 1
           ;;
       *)
-          echo "Aún en progreso... esperando 20 segundos"
-          sleep 20
+          echo "Aún en progreso... esperando 30 segundos"
+          sleep 30
           ;;
   esac
 done
-
-echo "Glue completado."
 sleep 2
 
 echo ""
@@ -103,7 +101,42 @@ aws emr wait cluster-running --cluster-id "$CLUSTER_ID" --region $REGION
 
 echo ""
 echo "Cluster en ejecución. Procesando Steps..."
-aws emr wait cluster-terminated --cluster-id "$CLUSTER_ID" --region $REGION
+STEP_IDS=$(aws emr list-steps \
+  --cluster-id "$CLUSTER_ID" \
+  --region $REGION \
+  --query "Steps[*].Id" --output text)
+
+echo "Steps detectados:"
+echo "$STEP_IDS"
+
+# Convertir a array
+read -ra STEPS_ARRAY <<< "$STEP_IDS"
+
+# Validación
+if [ ${#STEPS_ARRAY[@]} -lt 2 ]; then
+    echo "ERROR: No se detectaron los dos steps (ETL y Analytics)."
+    exit 1
+fi
+
+ETL_STEP_ID=${STEPS_ARRAY[0]}
+ANALYTICS_STEP_ID=${STEPS_ARRAY[1]}
+
+echo "ETL Step ID: $ETL_STEP_ID"
+echo "Analytics Step ID: $ANALYTICS_STEP_ID"
+
+echo "===== ESPERANDO FINALIZACIÓN DEL ETL ====="
+aws emr wait step-complete \
+  --cluster-id "$CLUSTER_ID" \
+  --step-id "$ETL_STEP_ID" \
+  --region $REGION
+echo "ETL completado correctamente."
+
+echo "===== ESPERANDO FINALIZACIÓN DEL ANALYTICS ====="
+aws emr wait step-complete \
+  --cluster-id "$CLUSTER_ID" \
+  --step-id "$ANALYTICS_STEP_ID" \
+  --region $REGION
+echo "Analítica completada correctamente."
 
 echo ""
 echo "===== EMR FINALIZADO (ETL + ANALYTICS OK) ====="
@@ -115,10 +148,22 @@ echo "===== 5. INVOCANDO LAMBDA DE EXPOSICIÓN (API) ====="
 aws lambda invoke \
   --function-name "$LAMBDA_EXPOSICION" \
   --region $REGION \
-  response_api.json >/dev/null 2>&1
+  /dev/null >/dev/null 2>&1
 
 echo "Lambda de exposición ejecutada."
 sleep 1
+
+echo ""
+
+echo "===== 6. OBTENIENDO URL PÚBLICA DE LA LAMBDA ====="
+
+echo ""
+echo "URL pública de la API (Lambda Function URL):"
+aws lambda get-function-url-config \
+  --function-name lambda-url-gateway \
+  --region $REGION \
+  --query "FunctionUrl" \
+  --output text
 
 echo ""
 echo "===== PIPELINE COMPLETO ====="
